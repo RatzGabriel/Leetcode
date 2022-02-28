15e44bfb49a2f2f3a50604d2b2c2648929101820
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-02-28 07:56:03 +0100

    Time: 112 ms (52.46%), Space: 48.3 MB (31.15%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.js
@@ -4,47 +4,11 @@
  * @return {boolean}
  */
 var kLengthApart = function(nums, k) {
-    // 1) Interpret the Prompt
-//         given binary array nums and integer k
-//          return true if all 1`s are at least k places away from each
-//
-// 2)
 
-//   I:nums[]
-
-//   O:boolean
-
-//   C:array length
-
-//   E: array not binary
-
-// 4) High Level Strategys
-
-//      1.iterate 
-    //     if 1
-    //      if last 1 return true
-     //      check if next 1 is at least k indices away
-      //      if no return false
-      //   if last 1 return true
-
-//      2. get all indice difference between 1s
-    //     if any of it is < k return false
-
-// 5) PseudoCode
-       // for nums i length
-       //   if nums[i] ===1
-       //     for nums j length 
-       //       if nums[j]===1
-       //     -1  nums[j]-nums[i] <k return false
-        //      if no nums[j] return true
-// 6) Implement the solution
-    
     for(let i=0;i<nums.length;i++){
-           console.log(nums[i])
         if(nums[i]===1){
          
             for(let j=i+1;j<nums.length;j++){
-                console.log(i,j)
                 if(nums[j]===1){
                     if((j-1)-i<k){
                         return false
