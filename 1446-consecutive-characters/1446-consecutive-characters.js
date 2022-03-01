d7149cc22a66406341315b1b912185238375986a
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-03-01 06:57:31 +0100

    Time: 111 ms (32.80%), Space: 42.2 MB (61.83%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.js
+++ 1446-consecutive-characters/1446-consecutive-characters.js
@@ -8,22 +8,13 @@ if(s.length===0)return 0
     let max=0;
     for(let i=0;i<s.length;i++){
         for(let j=i;j<s.length;j++){
-            console.log('new run')
             if(s[j]===s[i]){
-                console.log('run1')
                 if((j-i)+1>max){
-                console.log('run2')
                 max=(j-i)+1
             }
                 continue
-            }else {
-                console.log('break')
-                break}
+            }else break
         }
-    }
-    
-    
-    
-    
+    }  
 return max
 };
