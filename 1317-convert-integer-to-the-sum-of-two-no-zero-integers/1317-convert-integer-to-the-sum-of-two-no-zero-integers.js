63d0e74389f5bffe0812826d8221aeff2155e365
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-03-15 06:53:24 +0100

    Time: 808 ms (8.70%), Space: 55.7 MB (6.52%) - LeetHub

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -3,31 +3,8 @@
  * @return {number[]}
  */
 var getNoZeroIntegers = function(n) {
-    // 1) Interpret the Prompt
-//      find a solution where a+b=n 
-//      a and b should not contain any 0
 
-// 2)
-
-//   I:n
-
-//   O:array
-
-//   C: a and b should not contain any 0
-
-
-// 4) High Level Strategys
-
-//      1. iterate a for 10000
-    //     iterate b for 10000
-    //      if a+b=n return [a,b]
-    //      if a+b >n return
-    
-
-
-// 5) PseudoCode
 let regexZero=/([0])/g
-// 6) Implement the solution
     for(let i=0;i<10000;i++){
         for(let j=0;j<10000;j++){
             if(i.toString().match(regexZero)||j.toString().match(regexZero))continue
