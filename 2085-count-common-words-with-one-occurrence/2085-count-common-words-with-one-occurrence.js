40628fa64451b9e1fcd6104554f164b2be659165
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-03-16 07:18:24 +0100

    Time: 452 ms (5.17%), Space: 52 MB (5.17%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -4,43 +4,7 @@
  * @return {number}
  */
 var countWords = function(words1, words2) {
-    // 1) Interpret the Prompt
-//      in 2 arrays find all strigs that appear exactly once in each array
-//
-//
-// 2)
 
-//   I:string[] string[]
-
-//   O:number
-
-// 4) High Level Strategys
-
-//      1.iterate array1 check if string is unique in array 1
-//        if yes
-//        check if it is also unique in array 2
-//          if yes count++
-//          if no next word
-//
-
-//      2.reduce array1 and create array with objects {string,count}
-//        reduce array2 and create array with objects {string,count}
-//        iterate objects1[] 
-//          if objects1[].count===1
-//        iterate objects2[]
-//          if objects2[].count===1
-//            count++
-
-// 5) PseudoCode
-//          for array1.length 
-//            if string[i]===unique
-//              for array2.length
-//                 if string[j]===unique
-//                   count++
-//                 else continue
-
-// 6) Implement the solution
-    
     let uniqueArray1=words1.reduce((acc,sum)=>{
         if(acc[sum]===undefined){
         acc[sum]=1
