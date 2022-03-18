6d22d80ad60821451cef895915a2333cc722ae46
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-03-18 07:16:14 +0100

    Time: 382 ms (5.16%), Space: 49.9 MB (5.16%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.js
@@ -5,43 +5,6 @@
  */
 var countPairs = function(nums, k) {
 
-    // 1) Interpret the Prompt
-//      find pairs that meet conditions:
-//        0 <=i < j < n
-//        nums[i]==nums[j]
-//        i/j%k
-//
-// 2)
-
-//   I:nums[] , nums
-
-//   O:number
-
-//   C:
-
-//   E:
-
-// 3) Write Tests
-
-//
-
-// 4) High Level Strategys
-
-//      1.iterate array and check if a pair meets conditions
-
-//      2.
-
-// 5) PseudoCode
-    //       create count var
-    //       iterate array
-    //         iterate array
-    //           if nums[i]===nums[j]
-    //            if  0 <=i < j < n
-    //              if i*j%k===0
-    //                count++
-
-// 6) Implement the solution
-    
     let count=0;
     
     for(let i=0;i<nums.length;i++){
