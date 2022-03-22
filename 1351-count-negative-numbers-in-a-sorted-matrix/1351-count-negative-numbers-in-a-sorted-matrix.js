fe210d78ba6c21cbe67ed1909ad1b184073c47a6
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-03-22 06:52:10 +0100

    Time: 98 ms (35.29%), Space: 44.1 MB (43.91%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -12,31 +12,5 @@ var countNegatives = function(grid) {
         }
     }
     return count
-    // 1) Interpret the Prompt
-//          given "m x n matrix grid"-
-//           which is sorted non increasing order "both row-wise and column wise"
-//          return number of negative numbers in grid
-// 2)
 
-//   I:
-
-//   O:
-
-//   C:
-
-//   E:
-
-// 3) Write Tests
-
-//
-
-// 4) High Level Strategys
-
-//      1.
-
-//      2.
-
-// 5) PseudoCode
-
-// 6) Implement the solution
 };
