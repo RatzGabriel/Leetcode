b029648733e6a0277ff91486ffeb94341c10954f
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-04-04 07:46:31 +0200

    Time: 125 ms (59.95%), Space: 44.5 MB (73.38%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.js
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.js
@@ -5,16 +5,10 @@
  * @return {number[]}
  */
 var findKDistantIndices = function(nums, key, k) {
-   // 1) Interpret the Prompt
-//      resultArray
-//      given nums,k, key
-//      if k-distant-index=== |i-j|<=k && nums[j]==key
-//        resultArray.push(i) && break
     const resultArray=[];
     for(let i=0;i<nums.length;i++){
         for(let j=0;j<nums.length;j++){
         if(Math.abs((i-j))<=k && nums[j]===key){
-            console.log(i-j<=k,k,nums[j],key)
             resultArray.push(i);
             break;
         }
