ec0e86d2b8554f883dabceeeff1c5abcb97d7f2a
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-04-15 07:34:54 +0200

    Time: 102 ms (36.57%), Space: 42.5 MB (92.23%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,40 +3,9 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-    // 1) Interpret the Prompt
-// Given num[]
-// lucky integer=int that has frequency equal to value
-// return largest lucky int || -1
-
-// 2)
-
-//   I:num[]
-
-//
-
-//   O:num
-
-//   C:
-
-//   E:
-
-// 3) Write Tests
-//    [2,3,4,5,3,3]   3
-//
-
-// 4) High Level Strategys
-
-//      1.
-//        let count=-1;
-//        iterate
-//        if count of i === arr[i] & arr[i]>count count=arr[i]
-//        return count
-
-
 let count=-1;
     let b={}
-    for(let i=0;i<arr.length;i++){
-        
+    for(let i=0;i<arr.length;i++){ 
         let a=1;
         for(let j=i+1;j<arr.length;j++){
             if(b[arr[i]]===1){
@@ -51,17 +20,6 @@ let count=-1;
                 }
                 b[arr[i]]=1;
             a=1;    
-            }
-            
-        }
-    }
-    
+            }}}
     return count;
-    
-    
-    
-    
-    
-    
-    
 };
