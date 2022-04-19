0a21bb07bae1b2b6d35caed61db38329f21e6859
Gabriel <67792891+RatzGabriel@users.noreply.github.com>
2022-04-19 07:57:13 +0200

    Time: 341 ms (5.31%), Space: 50.1 MB (6.12%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -3,28 +3,7 @@
  * @return {number}
  */
 var findGCD = function(nums) {
-    // Find greatest common divisor of
-    // smallest number and largest number
-    // greatest common divisor=largest positive integer that
-    // evenly divides both numbers
-    // 
-    //
-    //
-    //I num[]
-    //O greatest common divisor , number
-    //
-    // Strategy:
-    // find smallest number 
-    // find greatest number
-    // find greatest divisor for both numbers
-    //
-    //
-    // sort it 
-    // a= sort[0]
-    // b=sort[last]
-    //
-    // counting down from b find greatest divisor
-    
+
     let sortedArray=nums.sort((a,b)=>{
         return a-b
     });
